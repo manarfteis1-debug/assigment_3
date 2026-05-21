f099b0ac4669b60d66c074034e7b876432f23cb7
manar <Manarfteis1@gmail.com>
2026-05-21 21:17:49 +0200

    random

--- assigment-3.cpp
+++ assigment-3.cpp
@@ -8,3 +8,16 @@ const int N = 8; // تحدد عدد الملكات وحجم المصفوفة
 const int POP_SIZE = 56; // عدد الحالات في كل جيل  
 const int MAX_GENERATIONS = 1000; //حددت اقصى حد للاجيال 
 const int MUTATION_RATE = 20; // نسبة حدود الطفرة 
+
+int randomSeed=7; //قيمة اولية للبذرة العشوائية 
+
+// دالة تغير في قيمة random في كل مره 
+int simpleRandom() {
+    randomSeed = (randomSeed * 37 + 11) % 1000;
+    return randomSeed;
+}
+
+// دالة ترجع رقم عشوائي يتراوح بين 0 و الرقم المدخل للدالة  
+int randomNumber(int maxValue) {
+    return simpleRandom() % maxValue;
+}
