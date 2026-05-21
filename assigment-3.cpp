64a199a38e81f529a552532d97079675b56fede1
manar <Manarfteis1@gmail.com>
2026-05-21 09:46:32 +0200

    consts

--- assigment-3.cpp
+++ assigment-3.cpp
@@ -1,1 +1,10 @@
-//
+#include <iostream>
+#include <fstream> 
+#include <ctime>
+using namespace std;
+
+// التوابت 
+const int N = 8; // تحدد عدد الملكات وحجم المصفوفة 
+const int POP_SIZE = 56; // عدد الحالات في كل جيل  
+const int MAX_GENERATIONS = 1000; //حددت اقصى حد للاجيال 
+const int MUTATION_RATE = 20; // نسبة حدود الطفرة 
